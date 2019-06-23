0847baf7b0097930e5bb258c5bc3d8d81ea596e5
TheHellBox <thehellbox11@gmail.com>
2019-06-23 16:37:36 +0500

    Use rustfmt, clean up the code

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,12 +1,18 @@
 extern crate openhmd_rs;
 use openhmd_rs::*;
 
-fn main(){
+fn main() {
     let context = Context::new();
     println!("{}", context.probe());
     let device = context.list_open_device(0);
     context.update();
     println!("OHMD_ROTATION_QUAT {:?}", device.get_rotation_quat());
-    println!("OHMD_SCREEN_HORIZONTAL_RESOLUTION {:?}", device.get_scr_res_w());
-    println!("OHMD_SCREEN_VERTICAL_RESOLUTION {:?}", device.get_scr_res_h());
+    println!(
+        "OHMD_SCREEN_HORIZONTAL_RESOLUTION {:?}",
+        device.get_scr_res_w()
+    );
+    println!(
+        "OHMD_SCREEN_VERTICAL_RESOLUTION {:?}",
+        device.get_scr_res_h()
+    );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,17 @@ extern crate openhmd_rs_sys;
 
 use openhmd_rs_sys::*;
 
-pub use openhmd_rs_sys::{ohmd_float_value, ohmd_string_value, ohmd_int_value};
+pub use openhmd_rs_sys::{ohmd_float_value, ohmd_int_value, ohmd_string_value};
 
-pub struct Context{
-    context: &'static ohmd_context
+pub struct Context {
+    context: &'static ohmd_context,
 }
 
-pub struct Device{
-    device: &'static ohmd_device
+pub struct Device {
+    device: &'static ohmd_device,
 }
 
-pub const SHADER_DISTORTION_VERT: &'static str  = r#"
+pub const SHADER_DISTORTION_VERT: &'static str = r#"
 version 120
 void main(void)
 {
@@ -70,196 +70,190 @@ void main()
 }
 "#;
 
-impl Context{
-    pub fn new() -> Context{
-        unsafe{
-            Context{
-                context: ohmd_ctx_create()
+impl Context {
+    pub fn new() -> Context {
+        unsafe {
+            Context {
+                context: ohmd_ctx_create(),
             }
         }
     }
 
-    pub fn probe(&self) -> i32{
-        unsafe{
-            ohmd_ctx_probe(self.context) as i32
-        }
+    pub fn probe(&self) -> i32 {
+        unsafe { ohmd_ctx_probe(self.context) as i32 }
     }
 
-    pub fn update(&self){
-        unsafe{
+    pub fn update(&self) {
+        unsafe {
             ohmd_ctx_update(self.context);
         }
     }
 
-    pub fn list_open_device(&self, index: i32) -> Device{
-        unsafe{
-            Device{
-                device: ohmd_list_open_device(self.context, index)
+    pub fn list_open_device(&self, index: i32) -> Device {
+        unsafe {
+            Device {
+                device: ohmd_list_open_device(self.context, index),
             }
         }
     }
 
-    pub fn get_error(&self) -> i32{
-        unsafe{
-            ohmd_ctx_get_error(self.context) as i32
-        }
+    pub fn get_error(&self) -> i32 {
+        unsafe { ohmd_ctx_get_error(self.context) as i32 }
     }
 
-    pub fn destroy(&self){
-        unsafe{
+    pub fn destroy(&self) {
+        unsafe {
             ohmd_ctx_destroy(self.context);
         }
     }
 
-    pub fn list_gets(&self, index: i32, otype: ohmd_string_value) -> &str{
+    pub fn list_gets(&self, index: i32, otype: ohmd_string_value) -> &str {
         use std::ffi::CStr;
-        unsafe{
+        unsafe {
             let raw = ohmd_list_gets(self.context, index, otype);
             CStr::from_ptr(raw).to_str().unwrap()
         }
     }
 }
 
-impl Drop for Context{
-    fn drop(&mut self){
+impl Drop for Context {
+    fn drop(&mut self) {
         self.destroy();
     }
 }
 
-impl Device{
-    pub fn getf(&self, otype: ohmd_float_value) -> Option<[f32; 16]>{
-        let mut out: [f32; 16] = [0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0];
-        unsafe{
-            match ohmd_device_getf(self.device, otype, &mut out){
+impl Device {
+    pub fn getf(&self, otype: ohmd_float_value) -> Option<[f32; 16]> {
+        let mut out: [f32; 16] = [0.0; 16];
+        unsafe {
+            match ohmd_device_getf(self.device, otype, &mut out) {
                 0 => return Some(out),
-                _ => return None
+                _ => return None,
             };
         }
     }
 
-    pub fn get_rotation_quat(&self) -> [f32; 4]{
-        let ohmd_orient = match self.getf(ohmd_float_value::OHMD_ROTATION_QUAT){
+    pub fn get_rotation_quat(&self) -> [f32; 4] {
+        let ohmd_orient = match self.getf(ohmd_float_value::OHMD_ROTATION_QUAT) {
             Some(x) => [x[0], x[1], x[2], x[3]],
-            _ => [0.0,0.0,0.0,0.0]
+            _ => [0.0; 4],
         };
         ohmd_orient
     }
 
-    pub fn get_position_vec(&self) -> [f32; 3]{
-        let ohmd_position = match self.getf(ohmd_float_value::OHMD_POSITION_VECTOR){
+    pub fn get_position_vec(&self) -> [f32; 3] {
+        let ohmd_position = match self.getf(ohmd_float_value::OHMD_POSITION_VECTOR) {
             Some(x) => [x[0], x[1], x[2]],
-            _ => [0.0,0.0,0.0]
+            _ => [0.0; 3],
         };
         ohmd_position
     }
 
-    pub fn get_view_matrix_l(&self) -> [f32; 16]{
-        let view_left = match self.getf(ohmd_float_value::OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX){
+    pub fn get_view_matrix_l(&self) -> [f32; 16] {
+        let view_left = match self.getf(ohmd_float_value::OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX) {
             Some(x) => x,
-            None => [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0]
+            None => [0.0; 16],
         };
         view_left
     }
 
-    pub fn get_view_matrix_r(&self) -> [f32; 16]{
-        let view_right = match self.getf(ohmd_float_value::OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX){
+    pub fn get_view_matrix_r(&self) -> [f32; 16] {
+        let view_right = match self.getf(ohmd_float_value::OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX) {
             Some(x) => x,
-            None => [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0]
+            None => [0.0; 16],
         };
         view_right
     }
 
-    pub fn get_proj_matrix_l(&self) -> [f32; 16]{
-        let oproj = match self.getf(ohmd_float_value::OHMD_LEFT_EYE_GL_PROJECTION_MATRIX){
+    pub fn get_proj_matrix_l(&self) -> [f32; 16] {
+        let oproj = match self.getf(ohmd_float_value::OHMD_LEFT_EYE_GL_PROJECTION_MATRIX) {
             Some(x) => x,
-            _ => [0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0]
+            _ => [0.0; 16],
         };
         oproj
     }
 
-    pub fn get_proj_matrix_r(&self) -> [f32; 16]{
-        let oproj = match self.getf(ohmd_float_value::OHMD_RIGHT_EYE_GL_PROJECTION_MATRIX){
+    pub fn get_proj_matrix_r(&self) -> [f32; 16] {
+        let oproj = match self.getf(ohmd_float_value::OHMD_RIGHT_EYE_GL_PROJECTION_MATRIX) {
             Some(x) => x,
-            _ => [0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0.0, 0.0, 0.0]
+            _ => [0.0; 16],
         };
         oproj
     }
 
-    pub fn get_scr_size_w(&self) -> f32{
-        let scr_size_w = match self.getf(ohmd_float_value::OHMD_SCREEN_HORIZONTAL_SIZE){
+    pub fn get_scr_size_w(&self) -> f32 {
+        let scr_size_w = match self.getf(ohmd_float_value::OHMD_SCREEN_HORIZONTAL_SIZE) {
             Some(x) => x[0],
-            _ => 0.149760
+            _ => 0.149760,
         };
         scr_size_w
     }
 
-    pub fn get_scr_size_h(&self) -> f32{
-        let scr_size_h = match self.getf(ohmd_float_value::OHMD_SCREEN_VERTICAL_SIZE ){
+    pub fn get_scr_size_h(&self) -> f32 {
+        let scr_size_h = match self.getf(ohmd_float_value::OHMD_SCREEN_VERTICAL_SIZE) {
             Some(x) => x[0],
-            _ => 0.093600
+            _ => 0.093600,
         };
         scr_size_h
     }
 
-    pub fn get_scr_res_w(&self) -> u32{
-        let scrw = match self.geti(ohmd_int_value::OHMD_SCREEN_HORIZONTAL_RESOLUTION){
+    pub fn get_scr_res_w(&self) -> u32 {
+        let scrw = match self.geti(ohmd_int_value::OHMD_SCREEN_HORIZONTAL_RESOLUTION) {
             Some(x) => x,
-            _ => 1280
+            _ => 1280,
         } as u32;
         scrw
     }
 
-    pub fn get_scr_res_h(&self) -> u32{
-        let scrh = match self.geti(ohmd_int_value::OHMD_SCREEN_VERTICAL_RESOLUTION){
+    pub fn get_scr_res_h(&self) -> u32 {
+        let scrh = match self.geti(ohmd_int_value::OHMD_SCREEN_VERTICAL_RESOLUTION) {
             Some(x) => x,
-            _ => 800
+            _ => 800,
         } as u32;
         scrh
     }
 
-    pub fn get_distortion_k(&self) -> [f32; 4]{
-        let distortion_k = match self.getf(ohmd_float_value::OHMD_UNIVERSAL_DISTORTION_K ){
+    pub fn get_distortion_k(&self) -> [f32; 4] {
+        let distortion_k = match self.getf(ohmd_float_value::OHMD_UNIVERSAL_DISTORTION_K) {
             Some(x) => [x[0], x[1], x[2], x[3]],
-            _ => [0.0,0.0,0.0,1.0]
+            _ => [0.0, 0.0, 0.0, 1.0],
         };
         distortion_k
     }
 
-    pub fn get_aberration_k(&self) -> [f32; 3]{
-        let aberration_k = match self.getf(ohmd_float_value::OHMD_UNIVERSAL_ABERRATION_K ){
-            Some(x) =>  [x[0], x[1], x[2]],
-            _ => [0.0,0.0,1.0]
+    pub fn get_aberration_k(&self) -> [f32; 3] {
+        let aberration_k = match self.getf(ohmd_float_value::OHMD_UNIVERSAL_ABERRATION_K) {
+            Some(x) => [x[0], x[1], x[2]],
+            _ => [0.0, 0.0, 1.0],
         };
         aberration_k
     }
 
-	pub fn setf(&self, otype: ohmd_float_value, value: &mut [f32; 16]) -> Option<bool>{
-        unsafe{
-            match ohmd_device_setf(self.device, otype, value){
+    pub fn setf(&self, otype: ohmd_float_value, value: &mut [f32; 16]) -> Option<bool> {
+        unsafe {
+            match ohmd_device_setf(self.device, otype, value) {
                 0 => return Some(true),
-                _ => return None
+                _ => return None,
             };
         }
     }
 
-    pub fn geti(&self, otype: ohmd_int_value) -> Option<i32>{
+    pub fn geti(&self, otype: ohmd_int_value) -> Option<i32> {
         let mut out: [i32; 1] = [0];
-        unsafe{
-            match ohmd_device_geti(self.device, otype, &mut out){
+        unsafe {
+            match ohmd_device_geti(self.device, otype, &mut out) {
                 0 => return Some(out[0]),
-                _ => return None
+                _ => return None,
             };
         }
     }
-    fn close(&self) -> i32{
-        unsafe{
-            ohmd_close_device(self.device) as i32
-        }
+    fn close(&self) -> i32 {
+        unsafe { ohmd_close_device(self.device) as i32 }
     }
 }
 
-impl Drop for Device{
-    fn drop(&mut self){
+impl Drop for Device {
+    fn drop(&mut self) {
         self.close();
     }
 }
